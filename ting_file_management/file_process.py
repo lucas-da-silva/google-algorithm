5ef936540d98adfd585b36d353709cedfd303f96
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-08 11:26:51 -0300

    feat: implements process function to adds read files

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.abstract_queue import AbstractQueue
+
+
+def process(path_file: str, instance: AbstractQueue):
+    file_already_added = any(
+        data["nome_do_arquivo"] == path_file for data in instance._data
+    )
+    if not file_already_added:
+        texts = txt_importer(path_file)
+        text_formatted = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(texts),
+            "linhas_do_arquivo": texts,
+        }
+        instance.enqueue(text_formatted)
+        print(text_formatted)
 
 
 def remove(instance):
