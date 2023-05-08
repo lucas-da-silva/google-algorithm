897dcdade14e50b6be019f9cba0a0ffda08d56a5
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-08 17:25:51 -0300

    feat: implements search_by_word function to return files with word and create build_file_occurrences function to avoid repetitions

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,25 +1,38 @@
 from ting_file_management.queue import Queue
 
 
+def build_file_occurrences(
+    file: dict, word: str, include_content=False
+) -> dict | None:
+    file_occurrences = {
+        "palavra": word,
+        "arquivo": file["nome_do_arquivo"],
+        "ocorrencias": [
+            {"linha": index + 1, "conteudo": line}
+            if include_content
+            else {"linha": index + 1}
+            for index, line in enumerate(file["linhas_do_arquivo"])
+            if word.lower() in line.lower()
+        ],
+    }
+    return file_occurrences if file_occurrences["ocorrencias"] else None
+
+
 def exists_word(word: str, instance: Queue) -> list[dict]:
     files = instance._data
-    filtered_files = []
+    return list(
+        filter(None, [build_file_occurrences(file, word) for file in files])
+    )
 
-    for file in files:
-        file_occurrences = {
-            "palavra": word,
-            "arquivo": file["nome_do_arquivo"],
-            "ocorrencias": [
-                {"linha": index + 1}
-                for index, line in enumerate(file["linhas_do_arquivo"])
-                if word.lower() in line.lower()
+
+def search_by_word(word: str, instance: Queue) -> list[dict]:
+    files = instance._data
+    return list(
+        filter(
+            None,
+            [
+                build_file_occurrences(file, word, include_content=True)
+                for file in files
             ],
-        }
-        if file_occurrences["ocorrencias"]:
-            filtered_files.append(file_occurrences)
-
-    return filtered_files
-
-
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+        )
+    )
