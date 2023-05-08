81146a0d90c0e777396f263f3eb1127b1149dc25
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-08 16:27:51 -0300

    feat: implements exists_word function to filter files by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue) -> list[dict]:
+    files = instance._data
+    filtered_files = []
+
+    for file in files:
+        file_occurrences = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, line in enumerate(file["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }
+        if file_occurrences["ocorrencias"]:
+            filtered_files.append(file_occurrences)
+
+    return filtered_files
 
 
 def search_by_word(word, instance):
