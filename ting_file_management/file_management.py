35ec35683bbb42b4ffcf0133f5c3fc695bb5cb95
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-08 10:17:18 -0300

    feat: implements txt_importer function to read a file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            texts = file.read()
+        return texts.split("\n")
+    except FileNotFoundError:
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
